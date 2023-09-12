5f1afc02faa64ac77a15dd116ef82bead0ab4820
Prosnis <144787794+Prosnis@users.noreply.github.com>
2023-09-12 16:27:28 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,155 @@
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksList');
+const emptyList = document.querySelector('#emptyList');
+
+
+let tasks = [];
+
+if (localStorage.getItem('tasks')){
+	tasks = JSON.parse(localStorage.getItem('tasks'));
+	tasks.forEach((task) => renderTask(task));
+ }
+
+
+
+checkEmptyList();
+
+form.addEventListener('submit', addTask)
+tasksList.addEventListener('click', deleteTask)  
+tasksList.addEventListener('click', doneTask) 
+
+function addTask(event) {
+	// отменяем отправку формы
+	event.preventDefault()
+
+	// доставем текст из поля ввода
+	const taskText = taskInput.value
+
+    // опаисываем задачу в виде объекта
+	const newTask = {
+		id: Date.now(),
+		text: taskText,
+		done: false,
+	};
+
+    // доьавляем объект в массив задач
+	tasks.push(newTask)
+
+	// Добавляем задачу в хранилище браузера
+	saveToLocalStorage()
+
+	renderTask(newTask);
+
+
+
+	// очищаем поле ввода и возвращаем фокус
+	taskInput.value = ''
+	taskInput.focus()
+
+	checkEmptyList()
+
+
+
+
+
+}
+
+function deleteTask(event) { 
+
+
+	// проверяем если клик был не по кнопке "удалить задачу"
+	if (event.target.dataset.action !== 'delete') return ;
+
+
+		// находим ближайщего родителя 'closest(по классу, id, тегу)'
+		const parentNode = event.target.closest('li')
+
+		// определяем id задачи
+		const id = Number(parentNode.id)
+
+		// находим индекс в массиве 
+		const index = tasks.findIndex((task) => task.id === id)
+
+		console.log(index)
+		// удаляем задачу из массива
+		tasks.splice(index, 1)
+
+			// Добавляем задачу в хранилище браузера
+			saveToLocalStorage()
+
+		// удаляем задачу из разметки
+		parentNode.remove()
+
+
+		checkEmptyList()
+
+
+
+
+
+}
+
+function doneTask(event) {
+
+	// проверяем если клик был не по кнопке " задача выполнена"
+	if(event.target.dataset.action !== 'done') return ; 
+
+
+		// находим ближайщего родителя 'closest(по классу, id, тегу)'
+		const parentNode = event.target.closest('.list-group-item');
+
+		// определеяем ID 
+		const id  = Number(parentNode.id);
+		const task = tasks.find((task) => task.id === id)
+		task.done = !task.done
+
+		// Добавляем задачу в хранилище браузера
+		saveToLocalStorage()
+
+		const taskTitle = parentNode.querySelector('.task-title');
+		taskTitle.classList.toggle('task-title--done')
+		
+
+}
+
+function checkEmptyList(){
+	if(tasks.length === 0) {
+		const emptyListHTML = `
+		<li id="emptyList" class="list-group-item empty-list">
+		<img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
+		<div class="empty-list__title">Список дел пуст</div>
+	</li>`
+	tasksList.insertAdjacentHTML('afterbegin', emptyListHTML)
+
+	}
+	if(tasks.length > 0) {
+		const emptyListEl = document.querySelector('#emptyList')
+		emptyListEl ? emptyListEl.remove() : null;
+	}
+}
+
+function saveToLocalStorage() {
+	localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function renderTask(task) { 
+
+	// формируем css класс
+	const cssClass = task.done ? 'task-title task-title--done' : 'task-title' ;
+
+	const taskHTML = `
+	<li id = "${task.id}" class="list-group-item d-flex justify-content-between task-item">
+			<span class="${cssClass}">${task.text}</span>
+			<div class="task-item__buttons">
+				<button type="button" data-action="done" class="btn-action">
+					<img src="./img/tick.svg" alt="Done" width="18" height="18">
+				</button>
+				<button type="button" data-action="delete" class="btn-action">
+					<img src="./img/cross.svg" alt="Done" width="18" height="18">
+				</button>
+			</div>
+	</li> `;
+	tasksList.insertAdjacentHTML('beforeend', taskHTML); 
+
+ }
